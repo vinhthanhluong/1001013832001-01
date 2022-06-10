782d25471f661413bc599bbe6add6551a89c776e
vinhthanh <vinhthanhluong@gmail.com>
2022-06-10 16:56:32 +0700

    update code scss

--- js/modules/common.js
+++ js/modules/common.js
@@ -131,11 +131,11 @@ $(function () {
             if ($('.header-menu').length) {
                 const _w = $(window).width();
                 if (_w < 751) {
-                    $('.dropdown > a').click(function () {
+                    $('.dropdown > span').click(function () {
                         $(this).toggleClass('open');
                         $(this).next('.sub-menu').stop().slideToggle();
                         $(this).closest('.dropdown').siblings().find('.sub-menu').stop().slideUp();
-                        $(this).closest('.dropdown').siblings().find('a').removeClass('open');
+                        $(this).closest('.dropdown').siblings().find('span').removeClass('open');
                     });
                 }
             }
